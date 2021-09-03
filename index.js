8885ffc3e8a39a071de80ee9015650fec3b332ed
MaryAninzo <aninzom211@gmail.com>
2021-09-03 11:39:26 -0400

    switch

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
+const header = document.getElementById("header")
 console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
 
+header.style.color = "red"
+
+
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
 
 
 /***** Deliverable 4 *****/
